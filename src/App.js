06f99cf3eebc8cf1bf54a025415bbd23b03b5517
forestus <guilhermepm164@gmail.com>
2021-07-14 22:21:47 -0300

    add css and img loaders to webpack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import "./App.css"
+import background from "./assets/5.jpg"
 export default function App() {
     return (
         <h1>
             Hello World!
-        </h1>
+        </h1>,
+        <img src={background}></img>
     )
 }
